2eba125a404b398581f82ea1b5b3758c199cf1fa
r3t51w <laphwng@gmail.com>
2015-12-23 18:58:22 +0000

    node section start

--- client/app.js
+++ client/app.js
@@ -1,20 +1,53 @@
-var app = angular.module('flapperNews',[]);
 
+var app = angular.module('flapperNews',['ui.router']);
+app.factory('posts',[function(){
+    //service body
+    var o={
+        posts:[
+            {title:'post 1',links:'',upvotes:5},
+            {title:'post 2',links:'',upvotes:2},
+            {title:'post 3',links:'',upvotes:15},
+            {title:'post 4',links:'',upvotes:9},
+            {title:'post 5',links:'',upvotes:5}
+            ]
+    };
+    return o;
+}]);
+app.config([
+    '$stateProvider',
+    '$urlRouterProvider',
+    function($stateProvider,$urlRouterProvider){
+        $stateProvider
+        .state('home',{
+            url:'/home',
+            templateUrl:'/home.html',
+            controller:'MainCtrl'
+        })
+        .state('posts',{
+            url:'/posts/{id}',
+            templateUrl:'/posts.html',
+            controller:'PostsCtrl'
+        });
+    $urlRouterProvider.otherwise('home');
+    }]);
 app.controller('MainCtrl',[
     '$scope',
-    function($scope){
+    'posts',
+    function($scope,posts){
         $scope.test='Hello world!';
-        $scope.posts=[
-            {title:'post 1',upvotes:5},
-            {title:'post 2',upvotes:2},
-            {title:'post 3',upvotes:15},
-            {title:'post 4',upvotes:9},
-            {title:'post 5',upvotes:4}
-        ];
+        $scope.posts=posts.posts;
     
     $scope.addPost=function(){
         if(!$scope.title ||$scope.title===''){return;}
-        $scope.posts.push({title:$scope.title, link:$scope.link, upvotes: 0});
+        $scope.posts.push({
+            title:$scope.title, 
+            link:$scope.link, 
+            upvotes: 0, 
+            comments: [
+    {author: 'Joe', body: 'Cool post!', upvotes: 0},
+    {author: 'Bob', body: 'Great idea but everything is wrong!', upvotes: 0}
+  ]
+        });
         $scope.title='';
         $scope.link='';
     };
@@ -23,3 +56,30 @@ app.controller('MainCtrl',[
     };
     }
 ]);
+
+app.controller('PostsCtrl',[
+    '$scope',
+    '$stateParams',
+    'posts',
+    function($scope, $stateParams, posts){
+        $scope.post=posts.posts[$stateParams.id];
+        
+        $scope.addPost=function(){
+          if($scope.body===''){return; }
+          $scope.post.comments.push({
+              body: $scope.body,
+              author: "user",
+              upvotes:0
+          });
+          $scope.body='';
+        };
+        $scope.addComment= function(){
+            if($scope.body===''){return;}
+            $scope.post.comments.push({
+                body:$scope.body,
+                author:'user',
+                upvotes:0
+            });
+            $scope.body='';
+        };
+    }]);
